b10b31fa5870b3074de55bf231243dcb01107933
Steffen André Langnes <steffenl.sw@gmail.com>
2017-03-10 02:45:45 +0100

    All current work

---
+++ CriminalIntent/app/src/main/java/com/example/sl/criminalintent/CrimeFragment.java
@@ -0,0 +1,62 @@
+package com.example.sl.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import static android.widget.CompoundButton.*;
+
+/**
+ * Created by Steffen on 2017-03-08.
+ */
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    mCrime.setTitle(mTitleField.getText().toString());
+                }
+            }
+        });
+
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+        return v;
+    }
+}
--- CriminalIntent/app/src/main/java/com/example/sl/criminalintent/CrimeListActivity.java
+++ CriminalIntent/app/src/main/java/com/example/sl/criminalintent/CrimeListActivity.java
@@ -1,11 +1,12 @@
 package com.example.sl.criminalintent;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by Steffen on 2017-03-08.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
