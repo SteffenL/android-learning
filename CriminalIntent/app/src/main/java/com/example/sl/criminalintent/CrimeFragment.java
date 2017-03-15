2b3a4d8bb74d4fc6ea29eff42abd5422bceb5f00
Steffen André Langnes <steffen.andre.langnes@invenia.no>
2017-03-15 13:17:59 +0100

    Fix issue with recycler view not updating
    Do not change the model when focus changes, but rather when text changes.
    
    Seems inefficient but it works, and this was what the book suggested in the first place anyway.

--- CriminalIntent/app/src/main/java/com/example/sl/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/example/sl/criminalintent/CrimeFragment.java
@@ -3,6 +3,8 @@ package com.example.sl.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,8 +25,6 @@ public class CrimeFragment extends Fragment {
     private static String ARG_CRIME_ID = "crime_id";
 
     private Crime mCrime;
-
-    private EditText mTitleField;
 
     static CrimeFragment newInstance(UUID crimeID) {
         Bundle args = new Bundle();
@@ -47,14 +47,20 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
-        mTitleField = (EditText)v.findViewById(R.id.crime_title);
-        mTitleField.setText(mCrime.getTitle());
-        mTitleField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        EditText titleField = (EditText)v.findViewById(R.id.crime_title);
+        titleField.setText(mCrime.getTitle());
+        titleField.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    mCrime.setTitle(mTitleField.getText().toString());
-                }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
 
