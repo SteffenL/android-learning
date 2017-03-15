619a97162d97c2f448b63a654874c916ddbbcd3c
Steffen André Langnes <steffen.andre.langnes@invenia.no>
2017-03-15 10:46:40 +0100

    Complete CriminalIntent and challenges for chapter 9

--- CriminalIntent/app/src/main/java/com/example/sl/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/sl/criminalintent/CrimeListFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,7 +46,7 @@ public class CrimeListFragment extends Fragment {
         public void bind(Crime crime) {
             mCrime = crime;
             mCrimeTitleTextView.setText(mCrime.getTitle());
-            mCrimeDateTextView.setText(mCrime.getDate().toString());
+            mCrimeDateTextView.setText(DateFormat.format(getString(R.string.crime_date_format), mCrime.getDate()));
             mCrimeSolvedImageView.setVisibility(mCrime.isSolved() ? View.VISIBLE : View.GONE);
         }
     }
